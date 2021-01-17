f0011f99e4c769432b660ff32792fd0fd7aaeea2
Saul Maldonado <samaldonado4@gmail.com>
2021-01-17 12:57:30 -0600

    feat: Add dev deps and config files
    Adds typescript
    Adds eslint
    Adds prettier
    Adds configs for eslint-airbnb-typescript
    Adds configs for prettier
    Adds declaration for module.css
    Changes index.js to index.tsx

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['airbnb-typescript', 'prettier/react'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'import/no-extraneous-dependencies': 0,
+  },
+};
